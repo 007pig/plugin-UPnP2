dc73af766907de072b55d6328932ff56521ab17a
Xiaoyu Huang <007pig@gmail.com>
2015-12-23 08:15:00 +0800

    Init commit

---
+++ src/main/java/plugins/UPnP2/UPnP2.java
@@ -0,0 +1,112 @@
+package plugins.UPnP2;
+
+import freenet.pluginmanager.FredPlugin;
+import freenet.pluginmanager.FredPluginThreadless;
+import freenet.pluginmanager.PluginRespirator;
+import org.fourthline.cling.UpnpService;
+import org.fourthline.cling.UpnpServiceImpl;
+import org.fourthline.cling.model.message.header.STAllHeader;
+import org.fourthline.cling.model.meta.LocalDevice;
+import org.fourthline.cling.model.meta.RemoteDevice;
+import org.fourthline.cling.registry.Registry;
+import org.fourthline.cling.registry.RegistryListener;
+
+/**
+ * Created by xiaoyu on 12/22/15. 2
+ */
+public class UPnP2 implements FredPlugin, FredPluginThreadless {
+
+    private PluginRespirator pr;
+
+    @Override
+    public void terminate() {
+        System.out.println("Test plugin ended");
+    }
+
+    @Override
+    public void runPlugin(PluginRespirator pr) {
+        this.pr = pr;
+
+        System.out.println("UPnP2 plugin started");
+
+        RegistryListener listener = new RegistryListener() {
+
+            public void remoteDeviceDiscoveryStarted(Registry registry,
+                                                     RemoteDevice device) {
+                System.out.println(
+                        "Discovery started: " + device.getDisplayString()
+                );
+            }
+
+            public void remoteDeviceDiscoveryFailed(Registry registry,
+                                                    RemoteDevice device,
+                                                    Exception ex) {
+                System.out.println(
+                        "Discovery failed: " + device.getDisplayString() + " => " + ex
+                );
+            }
+
+            public void remoteDeviceAdded(Registry registry, RemoteDevice device) {
+                System.out.println(
+                        "Remote device available: " + device.getDisplayString()
+                );
+            }
+
+            public void remoteDeviceUpdated(Registry registry, RemoteDevice device) {
+                System.out.println(
+                        "Remote device updated: " + device.getDisplayString()
+                );
+            }
+
+            public void remoteDeviceRemoved(Registry registry, RemoteDevice device) {
+                System.out.println(
+                        "Remote device removed: " + device.getDisplayString()
+                );
+            }
+
+            public void localDeviceAdded(Registry registry, LocalDevice device) {
+                System.out.println(
+                        "Local device added: " + device.getDisplayString()
+                );
+            }
+
+            public void localDeviceRemoved(Registry registry, LocalDevice device) {
+                System.out.println(
+                        "Local device removed: " + device.getDisplayString()
+                );
+            }
+
+            public void beforeShutdown(Registry registry) {
+                System.out.println(
+                        "Before shutdown, the registry has devices: "
+                                + registry.getDevices().size()
+                );
+            }
+
+            public void afterShutdown() {
+                System.out.println("Shutdown of registry complete!");
+
+            }
+        };
+
+        // This will create necessary network resources for UPnP right away
+        System.out.println("Starting Cling...");
+        UpnpService upnpService = new UpnpServiceImpl(listener);
+
+        // Send a search message to all devices and services, they should respond soon
+        upnpService.getControlPoint().search(new STAllHeader());
+
+        // Let's wait 10 seconds for them to respond
+        System.out.println("Waiting 10 seconds before shutting down...");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Release all resources and advertise BYEBYE to other UPnP devices
+        System.out.println("Stopping Cling...");
+        upnpService.shutdown();
+
+    }
+}
